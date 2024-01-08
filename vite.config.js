33b701870ca1b73f6c277e05418c57c418215b3e
Rajdev-ullash <rajdevullash111@gmail.com>
2024-01-08 14:04:12 +0600

    done

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/vite-deploy/",
+  resolve: {
+    alias: {
+      "@images": "/src/images",
+    },
+  },
 });
